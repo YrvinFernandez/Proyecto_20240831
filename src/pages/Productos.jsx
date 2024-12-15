15eccc69d7e8f947903b594093cf3d9465d61d70
Yrvin Fernandez <yrvin.fernandez@tecsup.edu.pe>
2024-12-14 23:40:18 -0500

    update with backend

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import { CarritoContext } from '../context/context'
-import data from '../data/data'
+
 import ProductCard from '../components/productCard'
 
 const Productos=()=>{
     const [, , addToCart] = useContext(CarritoContext)
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Función asíncrona para obtener datos
+        const fetchData = async () => {
+          try {
+            const response = await fetch('http://127.0.0.1:8000/tienda/productos/');
+            if (!response.ok) {
+                throw new Error('Network response for categories was not ok');
+            }
+            const result = await response.json();
+            console.log("Datos obtenidos:", result);
+            setData(result.results || []); // Actualizar el estado con los datos obtenidos
+          } catch (error) {
+            console.error("Error al obtener los datos:", error);
+          }
+        };
+        fetchData(); // Llamar la función asíncrona
+    }, [setData]);
 
     return(
         <div className="principal">
